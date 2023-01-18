739b22e947f8f8c6a9826c7a6b77d208f6b48fd5
Ye99-AungKhant <yeaungkhant077@gmail.com>
2023-01-18 11:48:49 +0630

    workfooter

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,4 +1,6 @@
 import Home_img from "../../assets/img/Home_img.jpg";
+import { NavLink } from "react-router-dom";
+
 
 const Home = (props) => {
 
@@ -26,34 +28,119 @@ const Home = (props) => {
             </section>
 
             {/* Work section */}
-            <section className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="my-5">
-                            <h3 className="text-center">Our Work Select Projects</h3>
-                            <div className="row row-cols-2 g-5">
+            <div className="bg-light">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-10">
+                            <section className="container">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <div className="my-5">
+                                            <h3 className="text-center">Our Work Select Projects</h3>
+                                            <div className="row row-cols-2 g-5">
 
-                                {work.map((work_ud) => {
-                                    return (
-                                        <div className="col" key={work_ud.id}>
-                                            <div className="card">
-                                                <img src={work_ud.src} className="card-img-top" alt="card" />
-                                                <div className="card-body">
-                                                    <h5 className="card-title">{work_ud.subtitle}</h5>
-                                                    <p className="card-text">{work_ud.desc}</p>
-                                                </div>
+                                                {work.map((work_ud) => {
+                                                    return (
+                                                        <div className="col" key={work_ud.id}>
+                                                            <div className="card">
+                                                                <img src={work_ud.src} className="card-img-top" alt="card" />
+                                                                <div className="card-body">
+                                                                    <h5 className="card-title">{work_ud.subtitle}</h5>
+                                                                    <p className="card-text">{work_ud.desc}</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })}
+
+
                                             </div>
+
                                         </div>
-                                    )
-                                })}
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* service section */}
+            <div className="bg-light">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-10">
+                            <section className="container">
+                                <div className="row">
+                                    <div className="col-lg-6">
+                                        <h4>Our Services</h4>
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit minus illum et itaque blanditiis id facilis voluptatum ipsum mollitia, iure sunt doloremque praesentium, ratione porro modi tenetur. Magni, autem.</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col">
+
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
+            {/* story section */}
+            <section className="container">
+                <div className="row justify-content-center">
+                    <div className="col-lg-10">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col">
+                                    <div className="text-center py-5 my-5">
+                                        <p>next</p>
+                                        <h4>Our story</h4>
+                                        <i className="fa-solid fa-arrow-right"></i>
+                                    </div>
+                                </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
             </section>
+
+            {/* footer section */}
+            <div className="border-top py-5">
+                <section className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-10">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col">
+                                        <div className="">
+                                            <NavLink to="/" className="text-decoration-none text-secondary me-4">Home</NavLink>
+                                            <NavLink to="/" className="text-decoration-none text-secondary me-4">Story</NavLink>
+                                            <NavLink to="/" className="text-decoration-none text-secondary me-4">Work</NavLink>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="text-center">
+                                            <h4>Repohub</h4>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="text-end">
+                                            <NavLink to="/" className="text-decoration-none text-secondary">Careers</NavLink>
+                                            <NavLink to="/" className="text-decoration-none text-secondary ms-4">Contact Us</NavLink>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
